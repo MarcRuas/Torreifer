de64b264ff2e3d4a872c92f8b0a97343fc5654e5
Marco Ruas <99102571+MarcRuas@users.noreply.github.com>
2023-09-16 13:54:49 -0400

    Adicionando um VerticalScroll

--- app/src/main/java/com/marco/torreifer/ui/view/HomeScreen.kt
+++ app/src/main/java/com/marco/torreifer/ui/view/HomeScreen.kt
@@ -3,6 +3,8 @@ package com.marco.torreifer.ui.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
